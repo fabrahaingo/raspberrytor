4a9978bd93b2c22ab3faef487b0146cf04710141
ntibi <nicolastibi@yahoo.fr>
2019-09-26 16:25:11 +0200

    ejs

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express()
 const PORT = 3000
 const db = require("./db")
 
+app.set('view engine', 'ejs');
+
 app.use('/img', express.static(__dirname + '/img'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html')
+app.get('/', async function (req, res) {
+  const raspberries = await db.all(`SELECT * FROM raspberry`)
+  res.render("index", { raspberries })
 })
 
 app.get('/db', async function (req, res) {
@@ -16,14 +19,9 @@ app.get('/db', async function (req, res) {
 
 app.post('/raspberry/delete', async function (req, res) {
   const raspberry_id = req.body.id
-  await db.run(
-    `DELETE FROM raspberry WHERE id = $1`,
-    [raspberry_id]
-  )
+  await db.run(`DELETE FROM raspberry WHERE id = $1`, [raspberry_id])
   return res.status(200)
 })
-
-app.use('/raspberry', raspberryRoutes)
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`)
